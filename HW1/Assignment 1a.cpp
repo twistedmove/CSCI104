55f547badef86e0687c2a3162cfb7b156e404e02
Bryan Chong <mail@bryanchong.com>
2013-08-28 00:24:04 -0700

    Updated part A

--- HW1/Assignment 1a.cpp
+++ HW1/Assignment 1a.cpp
@@ -1,14 +1,55 @@
+// Rewrite the central loop using recursion instead of a loop
+// Measure how long the processing of the loop takes with both methods (for loop and recursion).
+	// Do not include the time to read the input.
+// Infer roughly how much overhead you get from recursion compared to loops (as a percentage).	
+
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+#include <sstream>
 using namespace std;
 
 int main (void){
-	int *a;
+	int *a; // initialized pointer a
 	int n; // number of elements in the array a
 	int sum; // we'll use this for adding numbers
 
+	vector<int> numbers;
+	string temp;
 	/* here goes code to read n numbers into a, probably from a file
-	Make sure to allocate space to a
-	*/
+	Make sure to allocate space to a */
+	string fileName;
+	string temphold;
+	cout << "Please type in the file name and extension to import." << endl;
+	cin >> fileName;
+	ifstream importFile(fileName.c_str());
+	if (importFile.good()){
+		while (importFile.good()){
+			getline(importFile, temphold);
+			stringstream ss(temphold);
+			int integer;
+			ss >> integer;
+			numbers.push_back(integer);
+		}
+	}
+	else if(importFile.fail()){
+		cout << "Error: Check file name and location." << endl;
+		return 0;
+	} else if (importFile.eof()){
+		cout << "Reached end of file." << endl;
+	}
+	importFile.close();
+
+	n = numbers.size();
+
+	a = new int[n];
+
+	for (int i=0; i<n; i++){
+		a[i] = numbers[i];
+	}
 
 	// the next part is the loop you are supposed to replace by recursion.
 	sum = 0;
@@ -16,5 +57,5 @@ int main (void){
 	for (int i = 0; i < n; i ++)
 		sum += a[i]; // This is the end of the loop.
 		cout << sum << endl;
-		return 0;
-	}
+	return 0;
+}
--- HW1/Assignment 1b.cpp
+++ HW1/Assignment 1b.cpp
@@ -1,4 +1,14 @@
-/* various declarations here */
+// Rewrite the central loop using recursion instead of a loop
+// Measure how long the processing of the loop takes with both methods (for loop and recursion).
+	// Do not include the time to read the input.
+// Infer roughly how much overhead you get from recursion compared to loops (as a percentage).	
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+using namespace std;
+
 int main (void){
 	int *a;
 	int n; // number of elements in the array a
@@ -9,9 +19,9 @@ int main (void){
 	
 	// the next part is the loop you are supposed to replace by recursion.
 	m = a[0];
+
 	for (int i = 1; i < n; i ++)
-		if (a[i] > m) m = a[i];
-			// This is the end of the loop.
-		cout << m << endl;
+		if (a[i] > m) m = a[i]; // This is the end of the loop.
+			cout << m << endl;
 		return 0;
-	}
+}
