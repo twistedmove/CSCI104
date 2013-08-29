7b9865701242fdc00f62dc4336fa24fb2f3b4eca
Bryan Chong <mail@bryanchong.com>
2013-08-29 00:56:56 -0700

    Resyncing with file name changes. Edited numbers.txt file with additional lines of numbers to test.

--- HW1/Recursive1B.cpp
+++ HW1/Recursive1B.cpp
@@ -3,6 +3,10 @@
 	// Do not include the time to read the input.
 // Infer roughly how much overhead you get from recursion compared to loops (as a percentage).	
 
+// Bubble Sorting by Recursive
+
+// Average: 
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -15,12 +19,11 @@ using namespace std;
 int main (void){
 	int *a;
 	int n; // number of elements in the array a
-	int m; // we'll use this for the maximum
+	int m; // we'll use this for the maximum number
 
 	vector<int> numbers;
 	string temp;
-	/* here goes code to read n numbers into a, probably from a file
-	Make sure to allocate space to a */
+
 	string fileName;
 	string temphold;
 	cout << "Please type in the file name and extension to import." << endl;
@@ -41,17 +44,30 @@ int main (void){
 	}
 	importFile.close();
 
+	clock_t timeStart;
+	clock_t timeEnd;
+	timeStart = clock();
+
 	n = numbers.size();
 	a = new int[n];
 
-	
+	for (int i=0; i<n; i++){
+		a[i] = numbers[i];
+	}
+
 	// the next part is the loop you are supposed to replace by recursion.
 	m = a[0];
-
-	cout << m << endl;
+//	cout << m << endl;
 
 	for (int i = 1; i < n; i ++)
-		if (a[i] > m) m = a[i]; // This is the end of the loop.
-			cout << m << endl;
-		return 0;
+		if (a[i] > m) 
+			m = a[i]; // This is the end of the loop.
+		
+	cout << "The biggest number in the list is " << m << "." << endl;
+
+	timeEnd = clock();
+	cout << "It took " << (double)(timeEnd - timeStart)/CLOCKS_PER_SEC << " seconds to execute." << endl;
+
+
+	return 0;
 }
